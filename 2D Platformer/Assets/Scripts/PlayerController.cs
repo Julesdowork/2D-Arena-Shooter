53d4e0e5f23d36df45cfeec2a9baae4f3f76e99a
Julian McNeill <julesdowork@gmail.com>
2020-10-16 17:06:25 -0400

    Player Movement and Animations

---
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 40f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 1f;
+    [SerializeField] LayerMask whatIsGround;
+
+    bool facingRight = true;
+    bool isGrounded;
+
+    Animator animator;
+    Rigidbody2D rb2D;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        CheckIfGrounded();
+        Move();
+    }
+
+    void Move()
+    {
+        float xMove = Input.GetAxisRaw(TagManager.HORIZONTAL);
+        rb2D.velocity = new Vector2(xMove * speed, rb2D.velocity.y);
+        animator.SetFloat(TagManager.SPEED_PARAM, Mathf.Abs(xMove));
+
+        if (!facingRight && xMove > 0)
+            Flip();
+        else if (facingRight && xMove < 0)
+            Flip();
+    }
+
+    void Jump()
+    {
+        if (Input.GetButton(TagManager.JUMP) && isGrounded)
+        {
+            rb2D.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+    void CheckIfGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        animator.SetBool(TagManager.IS_GROUNDED_PARAM, isGrounded);
+    }
+}
---
+++ 2D Platformer/Assets/Scripts/TagManager.cs
@@ -0,0 +1,10 @@
+﻿public class TagManager
+{
+    // Tags
+    public static string HORIZONTAL = "Horizontal";
+    public static string JUMP = "Jump";
+
+    // Animation Parameters
+    public static string SPEED_PARAM = "Speed";
+    public static string IS_GROUNDED_PARAM = "IsGrounded";
+}
